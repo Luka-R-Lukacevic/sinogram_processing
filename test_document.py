db26127ab04059acb1c0d26dc769f51b82a6b345
Luka Lukacevic <luca.lukacevic@gmail.com>
2023-03-01 09:52:32 +0100

    added other test cases

--- test_document.py
+++ test_document.py
@@ -19,3 +19,30 @@ circle.plot_rec()
 
 
 circle.plot_sin()
+
+
+
+rec = CT_Object(np.zeros([256,256]))
+rec.add_rec_im(0,0,5,0.3,0.5)
+rec.plot()
+
+rec.plot_sin()
+rec.pert_sin(0,5)
+rec.plot_sin()
+rec.plot_rec()
+rec.rec_sin()
+rec.plot_sin()
+rec.plot_rec()
+
+
+twocircles = CT_Object(np.zeros([256,256]))
+twocircles.add_circ_im(0.5,0.5,0.2,1)
+twocircles.add_circ_im(-0.5,-0.5,0.2,3)
+twocircles.plot()
+twocircles.plot_sin()
+twocircles.pert_sin(0,5)
+twocircles.plot_sin()
+twocircles.plot_rec()
+twocircles.rec_sin()
+twocircles.plot_sin()
+twocircles.plot_rec()
